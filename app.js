64f547941ca5f221c25c561b3d696adc01f0f247
MP <14315607+rev1311@users.noreply.github.com>
2023-03-22 15:39:59 -0700

    updated api call endpoint

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post('/api/call', async (req, res) => {
+    const data = req.body;
+    console.log('Received data:', data);
   try {
-    const response = await axios.post('https://heroku-app-url.com/api/call', req.body);
+    const response = await axios.get(data.url, req.body);
     res.send(response.data);
+    console.log('Received data:', response.data);
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
